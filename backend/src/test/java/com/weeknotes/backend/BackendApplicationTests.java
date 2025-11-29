bad91e77fdaa6f8bdc4447b4434b3db2b1051199
02Kani <kanikaradag02@gmail.com>
2025-11-29 21:41:59 +0100

    spring setup

---
+++ backend/src/test/java/com/weeknotes/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weeknotes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
